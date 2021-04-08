d7b464fedf840390213eaab9b9bea0d40505b2aa
palak10017469 <80531480+palak10017469@users.noreply.github.com>
2021-04-08 10:34:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    classifier=ml5.imageClassifier('https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image',modelLoaded);
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    classifier.classify(video,gotResult);
+}
+
+function gotResult(error,results){
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_family_member").innerHTML=results[0].label;
+        document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
+
+function modelLoaded(){
+    console.log('modelLoaded');
+}
